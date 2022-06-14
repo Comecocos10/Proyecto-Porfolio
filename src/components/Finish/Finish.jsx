d7307175ac867c881fc1136f69671250bfad200d
Comecocos10 <ahp93524@gmail.com>
2022-06-14 07:34:16 +0200

    retoques + icon

--- src/components/Finish/Finish.jsx
+++ src/components/Finish/Finish.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Finish.css';
-import All from '../../recursos/ThreeD/finish.glb'
+import All from '../../recursos/ThreeD/finish.glb';
+import All2 from '../../recursos/finish.gif';
 
 export default function Finish() {
 
@@ -14,18 +15,8 @@ export default function Finish() {
 
     
     return <div className='finish'>
-        <h3 className='titel-finish'>Finish</h3>           
-        <model-viewer
-            id="reveal"
-            className="finish-model"
-            interpolation-decay="100" 
-            camera-orbit={cameraOrbit + 'deg'}
-            enable-pan src={All}  
-            environment-image="neutral" 
-            loading="eager" 
-            exposure="1.12" 
-            style={{ width: '500px', height: '300px' }}>
-            </model-viewer>
+        <h3 className='titel-finish'>Finish</h3>
+        <img className='video-finsh' src={All2} />           
         <p className='congratulation'> Congratualtion!</p>
         <p className='captured'> You have captured them all</p>
         <a className='email' href="mailto:andrea.hernan.93@gmail.com">¡¡You win my email!!</a>
