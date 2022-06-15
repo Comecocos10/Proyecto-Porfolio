0958a21d6f5da1363af4c9846dd0267fd4288dae
Comecocos10 <ahp93524@gmail.com>
2022-06-15 15:51:27 +0200

    Retoques

--- src/info/Photo/SliderPhoto.jsx
+++ src/info/Photo/SliderPhoto.jsx
@@ -5,7 +5,7 @@ function Trozo(props) {
     const [counter, setCounter] = useState(0);
     return (
         <div className='Sign-photo'>
-            <svg onClick={() => setCounter(counter === 0 ? props.data.length - 1 : counter - 1)} className='arrowseccion izqui' width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg onClick={() => setCounter(counter === 0 ? props.data.length - 1 : counter - 1)} className='arrowseccionphoto izqui' width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22 25L4 13L22 1V25Z" fill="black" />
             </svg>
             {
@@ -13,7 +13,7 @@ function Trozo(props) {
                     <img className='img-phto' src={props.data[counter].photo} />
                 </div>
             }
-            <svg onClick={() => setCounter(counter === props.data.length - 1 ? 0 : counter + 1)} className='arrowseccion drcha' width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg onClick={() => setCounter(counter === props.data.length - 1 ? 0 : counter + 1)} className='arrowseccionphoto drcha' width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22 25L4 13L22 1V25Z" fill="black" />
             </svg> 
         </div>
